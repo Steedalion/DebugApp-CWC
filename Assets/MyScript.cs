03db5a6cc56985ba4a8cfc52ce4dc221a133aec7
Alex <alex.steed88@gmail.com>
2021-01-06 15:58:03 +0900

    Working

---
+++ Assets/MyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+	
+    // Start is called before the first frame update
+    void Start()
+	{
+		StartCoroutine(Routine());
+	    Component c = GetComponent("MyScript");
+	    Debug.Log(c);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	IEnumerator Routine()
+	{
+		yield return new WaitForSeconds(5);
+		Debug.Log("Hello");
+		StartCoroutine(Routine());
+	}
+}
